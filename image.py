453c90f83c78d648acd3c821614959805e3ce7ea
ADARSH PATEL <adarsh2005patel@gmail.com>
2025-06-15 14:03:41 +0530

    Add save file dialog tkinter script

---
+++ image.py
@@ -0,0 +1,9 @@
+from tkinter import *
+win=Tk()
+photo=PhotoImage(file="sung-jinwoo-dragon-3840x2160-15351.jpg")
+lab=Label(win,image=photo,text="jin hoo sung",compound="top")
+lab.place(x=100,y=100)
+
+
+
+win.mainloop()
