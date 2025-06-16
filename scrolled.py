611e5b6e382c77241b60342c929aef89440c5145
ADARSH PATEL <adarsh2005patel@gmail.com>
2025-06-16 12:45:43 +0530

    scrolled

---
+++ scrolled.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter.scrolledtext import ScrolledText 
+win=Tk()
+
+scrolledText=ScrolledText(win,height=50,width=50)
+scrolledText.place(x=10,y=10)
+
+
+
+win.mainloop()
